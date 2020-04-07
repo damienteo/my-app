045916d962438ff67fa6d1c5974897d47ec1dfd5
damienteo <ksteo89@gmail.com>
2020-04-07 19:46:06 +0800

    Update About

--- components/views/about.jsx
+++ components/views/about.jsx
@@ -1,8 +1,8 @@
-import React from "react";
-import Layout from "../layout/layout";
-import Header from "../common/header";
-import Paragraph from "../common/paragraph";
-import ExternalLink from "../common/externalLink";
+import React from 'react'
+import Layout from '../layout/layout'
+import Header from '../common/header'
+import Paragraph from '../common/paragraph'
+import ExternalLink from '../common/externalLink'
 
 const AboutPage = () => {
   return (
@@ -11,24 +11,24 @@ const AboutPage = () => {
         <main>
           <Header text="About" />
           <Paragraph>
-            Hi, I am a hardworking peasant who likes working with intelligent
-            machines.
+            Hi, I am a hardworking peasant who enjoys working with intelligent
+            machines. Previously, I was a police officer.
           </Paragraph>
           <Paragraph>
-            This project was created with a mixture of:{" "}
+            This site was created with a mixture of:{' '}
             <ExternalLink url="https://reactjs.org/" label="React.js" />
-            , <ExternalLink url="https://nextjs.org/" label="Next.js" />, and{" "}
+            , <ExternalLink url="https://nextjs.org/" label="Next.js" />, and{' '}
             <ExternalLink url="https://material-ui.com/" label="Material UI" />.
             Essentially, it's just Javascript, HTML, and CSS.
           </Paragraph>
           <Paragraph>
-            As of the time of this writing, I am in Singapore and we are
-            undergoing what is called a CCB (
+            As of the time of this writing, we are undergoing what is called a
+            CCB (
             <ExternalLink
               url="https://www.straitstimes.com/singapore/a-new-normal"
               label="Covid-19 Circuit Breaker"
             />
-            ) in Singapore, and the DPM has just announced the{" "}
+            ) in Singapore, and the DPM has just announced the{' '}
             <ExternalLink
               url="https://www.singaporebudget.gov.sg/budget_2020/solidarity-budget"
               label="Solidarity Budget"
@@ -37,18 +37,23 @@ const AboutPage = () => {
           </Paragraph>
           <Paragraph>
             I completed a coding boot camp a year ago, and am currently employed
-            as a 'Junior Software Engineer'. I can be contacted on{" "}
+            as a{' '}
+            <ExternalLink
+              url="https://en.wikipedia.org/wiki/Nerd"
+              label="Junior Software Engineer"
+            />
+            . I can be contacted on{' '}
             <ExternalLink
               url="https://www.linkedin.com/in/damien-teo/"
               label="LinkedIn"
-            />{" "}
-            and{" "}
+            />{' '}
+            and{' '}
             <ExternalLink url="https://github.com/damienteo" label="Github" />.
           </Paragraph>
         </main>
       </Layout>
     </>
-  );
-};
+  )
+}
 
-export default AboutPage;
+export default AboutPage
