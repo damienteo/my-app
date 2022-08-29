b3ca09dacb4d70e7f1b27f19824310005fec20a1
damienteo <ksteo89@gmail.com>
2022-08-30 00:34:21 +0800

    Remove camelcase deprecated rule

--- lib/gtag.ts
+++ lib/gtag.ts
@@ -1,4 +1,3 @@
-/* eslint-disable @typescript-eslint/camelcase */
 export const GA_TRACKING_ID = process.env.NEXT_PUBLIC_GA_TRACKING_ID // This is your GA Tracking ID
 
 // Otherwise, you'll get "gtag is not defined errors" during dev and build (not an issue in production because the analytics script is loaded)
