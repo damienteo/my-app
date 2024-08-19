7c05abd8044b9fb6ae96b0936071d9c66b0a0d16
Damien Teo <damien@firerock.sg>
2024-08-19 15:32:56 +0800

    Add GA scripts

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,5 @@
 import React, { useEffect } from 'react'
+import Script from 'next/script'
 // import { usePathname } from 'next/navigation'
 import * as gtag from '../lib/gtag'
 import './globals.css'
@@ -27,7 +28,27 @@ export default function RootLayout({
 
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body>
+        {children} {/* Global Site Tag (gtag.js) - Google Analytics */}
+        <Script
+          src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GA_TRACKING_ID}`}
+          strategy="afterInteractive"
+        />
+        <Script
+          id="gtag-init"
+          strategy="afterInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', '${process.env.NEXT_PUBLIC_GA_TRACKING_ID}', {
+                page_path: window.location.pathname,
+              });
+            `,
+          }}
+        />
+      </body>
     </html>
   )
 }
