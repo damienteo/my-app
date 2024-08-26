a723400d5a31592771f1635dde4c00fd1a8c0116
Damien Teo <damien@firerock.sg>
2024-08-26 17:04:48 +0800

    Slight update

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -8,18 +8,18 @@ const config: Config = {
     './src/**/*.{js,jsx,ts,tsx}',
   ],
   theme: {
-    // extend: {
-    //   colors: {
-    //     primary: '#D21F3C',
-    //     'border-dark': '#2F3239',
-    //     'border-light': '#D8D8D8',
-    //     'section-bg-dark': '#1F2128',
-    //     'section-bg': '#EFF0F2',
-    //     'subtext-dark': '#848E9C',
-    //     subtext: '#545459',
-    //     error: '#CA1704',
-    //   },
-    // },
+    extend: {
+      //   colors: {
+      //     primary: '#D21F3C',
+      //     'border-dark': '#2F3239',
+      //     'border-light': '#D8D8D8',
+      //     'section-bg-dark': '#1F2128',
+      //     'section-bg': '#EFF0F2',
+      //     'subtext-dark': '#848E9C',
+      //     subtext: '#545459',
+      //     error: '#CA1704',
+      //   },
+    },
   },
   plugins: [],
 }
