a6c4382e2f6069eed3c3f81ceef5a0aed08eaf44
damienteo <damien.teo.ks@gmail.com>
2025-04-23 14:37:56 +0800

    Remove the wrapping of button in Link

--- app/components/common/links/ButtonLink.tsx
+++ app/components/common/links/ButtonLink.tsx
@@ -13,13 +13,13 @@ export const ButtonLink: React.FunctionComponent<ButtonLinkProps> = ({
   onClick,
 }) => {
   return (
-    <Link href={url} as={url}>
-      <button
-        className="block my-2 text-white hover:bg-[#282c35] rounded-lg px-3 py-2"
-        onClick={onClick}
-      >
-        {text}
-      </button>
+    <Link
+      href={url}
+      as={url}
+      className="block my-2 text-white hover:bg-[#282c35] rounded-lg px-3 py-2"
+      onClick={onClick}
+    >
+      {text}
     </Link>
   )
 }
